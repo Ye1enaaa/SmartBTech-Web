92f152b465f370993098ef0bea7e128473089532
unknown <avilasonson@gmail.com>
2024-02-28 01:03:28 +0800

    fix :hammer::Dashboard websocket data fixed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { faBolt, faTriangleExclamation, faPlug } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import io from 'socket.io-client';
 import USTPLogo from '/ustp.png';
+
 const Dashboard = () => {
+  const [data, setData] = useState({ power: 0, voltage: 0, current: 0 });
+
+  useEffect(() => {
+    const socket = io('http://192.168.254.111:5000');
+    socket.on('message', newData => {
+      setData(JSON.parse(newData));
+    });
+    
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  // Move the console log here to ensure logging the updated state
+  console.log(data);
+
   return (
     <div className="min-h-screen bg-gray-100 relative">
-      {/* <div className="absolute inset-0 z-0" style={{ backgroundImage: `url(${USTPLogo})`, backgroundSize: '400px', backgroundRepeat:'no-repeat' }}></div> */}
-      {/* <div className="justify-center z-0" style={{ backgroundImage: `url(${USTPLogo})`, backgroundSize: '400px', backgroundRepeat:'no-repeat' }}></div> */}
       {/* Top Bar */}
       <div className="bg-green-500 py-4 text-white text-center">
-        <h1 className="text-2xl font-bold" style={{fontFamily: 'Poppins'}}>SmartBTech USTP: Smart Building Technology</h1>
+        <h1 className="text-2xl font-bold" style={{ fontFamily: 'Poppins' }}>SmartBTech USTP: Smart Building Technology</h1>
       </div>
       {/* Main Content */}
       <div className="container mx-auto mt-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {/* Power Box */}
           <div className="bg-white p-4 rounded-lg shadow-md h-48">
-            <h2 className="text-lg font-semibold mb-2" style={{fontFamily:'Poppins'}}>Power [W]</h2>
+            <h2 className="text-lg font-semibold mb-2" style={{ fontFamily: 'Poppins' }}>Power [W]</h2>
             <div className='flex justify-end mr-6 space-x-20'>
-              <div className='text-8xl' style={{fontFamily:'Poppins'}}>137</div>
-              <FontAwesomeIcon icon={faBolt} size='6x' style={{color:'#8B8000'}}/>
+              <div className='text-8xl' style={{ fontFamily: 'Poppins' }}>{data.power}</div>
+              <FontAwesomeIcon icon={faBolt} size='6x' style={{ color: '#8B8000' }} />
             </div>
           </div>
 
           {/* Voltage Box */}
           <div className="bg-white p-4 rounded-lg shadow-md h-48">
-            <h2 className="text-lg font-semibold mb-2" style={{fontFamily:'Poppins'}}>Voltage [V]</h2>
+            <h2 className="text-lg font-semibold mb-2" style={{ fontFamily: 'Poppins' }}>Voltage [V]</h2>
             <div className='flex justify-end mr-6 space-x-20'>
-              <div className='text-8xl' style={{fontFamily:'Poppins'}}>137</div>
-              <FontAwesomeIcon icon={faTriangleExclamation} size='6x' style={{color:'#FF0000'}}/>
+              <div className='text-8xl' style={{ fontFamily: 'Poppins' }}>{data.voltage}</div>
+              <FontAwesomeIcon icon={faTriangleExclamation} size='6x' style={{ color: '#FF0000' }} />
             </div>
           </div>
 
           {/* Current Box */}
           <div className="bg-white p-4 rounded-lg shadow-md h-48">
-            <h2 className="text-lg font-semibold mb-2" style={{fontFamily:'Poppins'}}>Current [A]</h2>
+            <h2 className="text-lg font-semibold mb-2" style={{ fontFamily: 'Poppins' }}>Current [A]</h2>
             <div className='flex justify-end mr-6 space-x-20'>
-              <div className='text-8xl' style={{fontFamily:'Poppins'}}>137</div>
-              <FontAwesomeIcon icon={faPlug} size='6x' style={{color:'#8B8000'}}/>
+              <div className='text-8xl' style={{ fontFamily: 'Poppins' }}>{data.current}</div>
+              <FontAwesomeIcon icon={faPlug} size='6x' style={{ color: '#8B8000' }} />
             </div>
           </div>
         </div>
