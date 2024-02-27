68bfdd2c3a40b349db639effe0e494eb45470d74
unknown <avilasonson@gmail.com>
2024-02-27 10:53:19 +0800

    cosmetics :lipstick::added new logos for power,voltage,current

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { faBolt } from '@fortawesome/free-solid-svg-icons';
+import { faBolt, faTriangleExclamation, faPlug } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import USTPLogo from '/ustp.png';
 const Dashboard = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 relative">
+      {/* <div className="absolute inset-0 z-0" style={{ backgroundImage: `url(${USTPLogo})`, backgroundSize: '400px', backgroundRepeat:'no-repeat' }}></div> */}
+      {/* <div className="justify-center z-0" style={{ backgroundImage: `url(${USTPLogo})`, backgroundSize: '400px', backgroundRepeat:'no-repeat' }}></div> */}
       {/* Top Bar */}
       <div className="bg-green-500 py-4 text-white text-center">
         <h1 className="text-2xl font-bold" style={{fontFamily: 'Poppins'}}>SmartBTech USTP: Smart Building Technology</h1>
       </div>
-
       {/* Main Content */}
       <div className="container mx-auto mt-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -26,7 +28,7 @@ const Dashboard = () => {
             <h2 className="text-lg font-semibold mb-2" style={{fontFamily:'Poppins'}}>Voltage [V]</h2>
             <div className='flex justify-end mr-6 space-x-20'>
               <div className='text-8xl' style={{fontFamily:'Poppins'}}>137</div>
-              <FontAwesomeIcon icon={faBolt} size='6x' style={{color:'#8B8000'}}/>
+              <FontAwesomeIcon icon={faTriangleExclamation} size='6x' style={{color:'#FF0000'}}/>
             </div>
           </div>
 
@@ -35,7 +37,7 @@ const Dashboard = () => {
             <h2 className="text-lg font-semibold mb-2" style={{fontFamily:'Poppins'}}>Current [A]</h2>
             <div className='flex justify-end mr-6 space-x-20'>
               <div className='text-8xl' style={{fontFamily:'Poppins'}}>137</div>
-              <FontAwesomeIcon icon={faBolt} size='6x' style={{color:'#8B8000'}}/>
+              <FontAwesomeIcon icon={faPlug} size='6x' style={{color:'#8B8000'}}/>
             </div>
           </div>
         </div>
